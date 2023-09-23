930aa2b3972fe5a99465ed10b4b7353671441380
Zhukova Alla <90785698+zhuzzzhha@users.noreply.github.com>
2023-09-23 17:25:04 +0300

    Update and rename 1.py to task_1.py

--- lab_2/task_1.py
+++ lab_2/task_1.py
@@ -6,13 +6,16 @@ def newton_method(f, x_0, eps=1e-10):
     x = sp.symbols('x')
     f_prime = sp.diff(f, x)
     f_prime_func = sp.lambdify(x, f_prime, 'numpy')
+    fpx = f_prime_func(x_0)
     fx = f.subs(x, x_0)
-    while abs(fx) >= eps:
+    x_1 = x_0 - fx / fpx
+    while abs(x_1 - x_0) >= eps:
+        x_0 = x_1
         fx = f.subs(x, x_0)
         fpx = f_prime_func(x_0)
         if fpx == 0:
             raise ValueError('Производная равна нулю. Метод Ньютона не сходится.')
-        x_0 = x_0 - fx / fpx
+        x_1 = x_0 - fx / fpx
     return float(x_0)
 
 
