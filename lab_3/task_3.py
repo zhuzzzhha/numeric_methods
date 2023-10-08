783902959b903d1c49e90acb6f9f91e598dafd4e
Zhukova Alla <90785698+zhuzzzhha@users.noreply.github.com>
2023-10-08 10:03:37 +0300

    Update task_3.py

--- lab_3/task_3.py
+++ lab_3/task_3.py
@@ -1,33 +1,31 @@
 import numpy as np
 
-
 def gauss_method(matrix, vector):
     n = len(matrix)
+    matrix = np.hstack((matrix, vector.reshape(-1,1)))
 
     # Прямой ход
     for i in range(n):
-        max_row = np.argmax(np.abs(matrix[i:, i])) + i
-        matrix[[i, max_row]] = matrix[[max_row, i]]
-        vector[i], vector[max_row] = vector[max_row], vector[i]
+        pivot = matrix[i, i]
 
         for k in range(i + 1, n):
-            factor = matrix[k, i] / matrix[i, i]
+            factor = matrix[k, i] / pivot
             matrix[k, i:] -= factor * matrix[i, i:]
-            vector[k] -= factor * vector[i]
 
     # Обратный ход
+    n = len(matrix)
     x = np.zeros(n)
     for i in range(n - 1, -1, -1):
-        x[i] = (vector[i] - np.dot(matrix[i, i + 1:], x[i + 1:])) / matrix[i, i]
+        x[i] = (matrix[i, -1] - np.dot(matrix[i, i + 1:n], x[i + 1:n])) / matrix[i, i]
 
     return x
 
 
-A = np.array([[3, 2, -1],
-              [2, -2, 4],
-              [-1, 0.5, -1]], dtype=float)
+A = np.array([[2, 1, 4],
+              [3, 2, 1],
+              [1, 3, 3]], dtype=float)
 
-b = np.array([1, -2, 0], dtype=float)
+b = np.array([16, 10, 16], dtype=float)
 
 result = gauss_method(A, b)
 print("Решение системы уравнений:", result)
