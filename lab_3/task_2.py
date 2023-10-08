5ce6c3ebeea06510ccde2c72fb213f7473957460
Zhukova Alla <90785698+zhuzzzhha@users.noreply.github.com>
2023-10-08 10:29:47 +0300

    Update task_2.py

--- lab_3/task_2.py
+++ lab_3/task_2.py
@@ -1,20 +1,15 @@
-#поиск ведущего элемента матрицы
+#выбор ведущего элемента матрицы
 import numpy as np
+def search_pivot(matrix):
 
-def find_pivot_element(matrix, row, col):
-    max_row = row
     n = len(matrix)
+    for i in range(n):
+        # Поиск максимального элемента в столбце под главной диагональю
+        max_row = np.argmax(np.abs(matrix[i:, i])) + i
+        matrix[[i, max_row]] = matrix[[max_row, i]]
+    return matrix
 
-    for i in range(row + 1, n):
-        if abs(matrix[i, col]) > abs(matrix[max_row, col]):
-            max_row = i
-
-    return max_row, col
-
-# Пример использования
 A = np.array([[2, 1, -1],
               [0, -1, 2],
-              [1, 1, -2]])
-
-row, col = find_pivot_element(A, 0, 0)
-print(f"Ведущий элемент: {A[row, col]}")
+              [3, 1, -2]])
+print(search_pivot(A))
